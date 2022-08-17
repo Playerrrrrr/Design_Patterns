f52f5352a90a5df91a1ee2dd7f7a432ffd49222f
PineappleKnife <876942392@qq.com>
2022-08-17 19:05:03 +0800

    Pop file
    pop

--- Hellow.cpp
+++ Hellow.cpp
@@ -2,6 +2,7 @@
 #include<unordered_map>
 
 int main(){
+    std::cout<<"Stash"<<std::endl;
     std::cout<<"修改"<<std::endl;
     std::cout<<"可以push了？？"<<std::endl;
     std::cout<<"就当练习英文"<<std::endl;
